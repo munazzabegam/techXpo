ed3bfdb4c3d92f844584c869de88eb971b940f51
azlanajju <muhammedazlan11@gmail.com>
2024-02-02 23:31:33 +0530

    chnged resposive view for event page

--- Server/config.php
+++ Server/config.php
@@ -1,4 +1,6 @@
 <?php
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
 
 $conn = new mysqli("localhost", "u214227927_PaceFest", "PaceFest2023", "u214227927_paceFest");
 // $conn = new mysqli("localhost", "root", "", "paceFest");
@@ -9,5 +11,5 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
-// error_reporting(E_ALL);
-// ini_set('display_errors', 1);
+
+<></>
